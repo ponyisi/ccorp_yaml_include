a9e8acd228834f681ffe5d765f8ff46952431d12
Marina Lalazarova <Marina_Lalazarova@epam.com>
2020-06-30 12:29:36 +0300

    version was updated to 0.0.3 and readme file was updated with new information.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ccorp-yaml-include-relative-path",
-    version="0.0.1",
+    version="0.0.2",
     author="Marina Lalazarova",
     author_email="marsolo@mail.ru",
     description="Exactly ccorp-yaml-include package with a bug fix and support of relative paths for included aliases",
